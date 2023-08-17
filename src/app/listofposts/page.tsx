89024110611b9ed474d3458d76b9b24ede7729b2
Jose Torres <vidojam@gmail.com>
2023-08-17 10:28:08 -0400

    project completion

--- src/app/listofposts/page.tsx
+++ src/app/listofposts/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const getPostsData = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts")
   return res.json()
@@ -8,13 +10,30 @@ const getUsersData = async () => {
   return res.json()
 }; 
 
+const getDogData = async () => {
+  // const res = await fetch("https://dog.ceo/api/breeds/image/random", {cache: "no-store"})
+  const res = await fetch("https://dog.ceo/api/breeds/image/random", {
+    next: {
+      revalidate: 3, // photo change after 3 seconds
+    }
+  });
+  return res.json()
+}; 
+
 
 export default async function ListOfPosts() {
-  const [posts, users] = await Promise.all([getPostsData(), getUsersData()]);
+  const [posts, users, dog] = await Promise.all([
+    getPostsData(), 
+    getUsersData(),
+    getDogData(),
+  ]);
 
-  console.log(users);
+  // console.log(users);
+  // console.log(posts);
+
   return (
    <div>
+    <Image src={dog.message} alt="dog" width={300} height={300}/>
     {posts.map((post: any) => {
       return <p>{posts.title}</p>;
       })}
